6ce226969d214611e14809c504c8a9aaf81a1d3b
Gunko Vika <vikahunko@icloud.com>
2024-11-29 23:43:22 +0300

    Add files via upload

---
+++ Lab_2/test.cpp
@@ -0,0 +1,42 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#include <gtest/gtest.h>
+#include <vector>
+#include <windows.h>
+#include <thread>
+#include <mutex>
+#include "Main.cpp" // Замените на имя вашего файла с основным кодом
+#ifndef TEST_MODE
+
+
+TEST(MinMaxTest, BasicAssertions) {
+    Data a;
+    a.numbs = { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
+
+    HANDLE hMin_Max = CreateThread(nullptr, 0, Min_Max, &a, 0, nullptr);
+    WaitForSingleObject(hMin_Max, INFINITE);
+
+    EXPECT_EQ(a.min, 1);
+    EXPECT_EQ(a.max, 9);
+
+    CloseHandle(hMin_Max);
+}
+
+// Тест для функции Average
+TEST(AverageTest, BasicAssertions) {
+    Data b;
+    b.numbs = { 3, 1, 4, 1, 5, 9 };
+
+    HANDLE hAverage = CreateThread(nullptr, 0, Average, &b, 0, nullptr);
+    WaitForSingleObject(hAverage, INFINITE);
+
+    EXPECT_DOUBLE_EQ(b.average, 3.833333); // Ожидаемое среднее значение
+
+    CloseHandle(hAverage);
+}
+
+//условная компиляция для предотвращения конфликта с main
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#endif
