be4db61a25921b131cf20f0b606fc57e174b5521
Gunko Vika <vikahunko@icloud.com>
2024-12-30 18:38:40 +0300

    Update Header.h

--- Lab_5/Header.h
+++ Lab_5/Header.h
@@ -1,18 +1,8 @@
 #pragma once
-#include <iostream>
 
-struct employee {
-    int num;           // ID 
-    char name[10];     
-    double hours;      
-
-    employee() : num(0), hours(0) {
-        strcpy_s(name, "Unnamed"); 
-    }
-
-    employee(const employee& emp) {
-        num = emp.num; 
-        hours = emp.hours; 
-        strcpy_s(name, emp.name); 
-    }
+struct employee
+{
+    int num; //ID
+    char name[10];
+    double hours;
 };
